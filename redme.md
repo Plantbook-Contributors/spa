3a61ea885dfedf43833b1cebbdd71edec10034cc
Евгений Дугин <duginea@mail.ru>
2021-05-23 03:42:25 +0500

    add btn

--- redme.md
+++
@@ -1,6 +0,0 @@
-npm init
-npm install --save-dev webpack
-webpack.config.js
-folder src/index.js
-npm install -D webpack-cli
-npm install webpack-dev-server --save-dev
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+ 
+    <form action="">
+        <button type="submit">SignIn</button>
+    </form>
+
+  );
+}
+
+export default App;
