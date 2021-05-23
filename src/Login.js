9be44a901885b74c4eee969041d24cdd2c073f5d
Евгений Дугин <duginea@mail.ru>
2021-05-24 02:05:41 +0500

    add page Login and Register (npm start)

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Sign up to Plantbook</h2>
+                <p>Hello Mauris sem velit, vehicula eget sodales vitae,
+                    rhoncus eget sapien:</p>
+                </div>
+        );
+    }
+}
+
+export default Login;
---
+++ src/Register.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Register to</h2>
+                <p>Mauris sem velit, vehicula eget sodales vitae,
+                    rhoncus eget sapien:</p>
+                </div>
+        );
+    }
+}
+
+export default Register;
